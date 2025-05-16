0b9db05f506cb1d69b996659fe1ddb884b901ae3
Brian ODonnell <bodonne3@depaul.edu>
2025-05-15 19:40:10 -0500

    new edits

--- demo/src/main/java/csc402/week5/LinearithmicTime.java
+++ demo/src/main/java/csc402/week5/LinearithmicTime.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 public class LinearithmicTime {
     public static void main(String[] args) {
         int[] array = {5, 3, 1, 4, 2};
+        IntegerMergeSort(array, 0, array.length-1);
+        System.out.println(Arrays.toString(array));
         String[] arrayString = {"Brian", "Alice", "David", "Cashew", "Eve", "Walnut"};
         ProfessionalSportsTeam[] teams = {
                 new ProfessionalSportsTeam("Bears", "Chicago", "Football"),
@@ -19,7 +21,10 @@ public class LinearithmicTime {
                 new ProfessionalSportsTeam("Fire", "Chicago", "Soccer")
         };
 
+        
+        
         //Bears
+        
         teams[0].setWins(5);
         teams[0].setLosses(12);
         //Cubs
@@ -45,6 +50,69 @@ public class LinearithmicTime {
 
         mergeSort(teams, 0, teams.length - 1);
         System.out.println(Arrays.toString(teams));
+    }
+
+    public static void IntegerMergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Recursively sort the left and right halves
+            IntegerMergeSort(array, left, mid);
+            IntegerMergeSort(array, mid + 1, right);
+
+            // Merge the sorted halves
+            IntegerMerge(array, left, mid, right);
+        }
+    }
+
+    public static void IntegerMerge(int[] array, int left, int mid, int right) {
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = array[mid + 1 + j];
+        }
+
+        // Merge the temporary arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[] if any
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[] if any
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
     }
 
     public static void mergeSort(ProfessionalSportsTeam[] array, int left, int right) {
--- demo/src/main/java/csc402/week5/SportsTeamsComparator.java
+++ demo/src/main/java/csc402/week5/SportsTeamsComparator.java
@@ -5,14 +5,29 @@ public class SportsTeamsComparator implements Comparator<ProfessionalSportsTeam>
     @Override
     public int compare(ProfessionalSportsTeam team1, ProfessionalSportsTeam team2) {
 
-        double winPerc = (team1.getWins() / (team1.getLosses() + team1.getWins()));
-        double winPerc2 = (team2.getWins() / (team2.getLosses() + team2.getWins()));
+        // double winPerc = (team1.getWins() / (team1.getLosses() + team1.getWins()));
+        // double winPerc2 = (team2.getWins() / (team2.getLosses() + team2.getWins()));
 
-        if (winPerc > winPerc2) {
-            return -1;
-        } else if (winPerc < winPerc2) {
+        // if (winPerc < winPerc2) {
+        //     return -1;
+        // } else if (winPerc > winPerc2) {
+        //     return 1;
+        // } else {
+        //     return 0;
+        // }
+
+        String teamSport1 = team1.getSport();
+        String teamSport2 = team2.getSport();
+
+        int compare = teamSport1.compareTo(teamSport2);
+
+        if( compare < 1){
             return 1;
-        } else {
+        }
+        else if(compare > 1){
+            return -1;
+        }
+        else {
             return 0;
         }
     }
